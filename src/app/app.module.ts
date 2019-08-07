764e818735923909cac3ffe58c001ebf531c6cd2
chonseng <chonseng38@gmail.com>
2019-08-06 22:18:51 -0700

    Add routing and bare modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StarterComponent } from './starter/starter.component';
+import { GameComponent } from './game/game.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StarterComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
