eec4525de40b513aee86ef868649cc2de841d0e5
chonseng <chonseng38@gmail.com>
2019-08-07 23:27:36 -0700

    Add start game logic and game board

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -6,10 +6,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+  size: number = 10
+  numOfPlayers: number = 4
+  // Game Status
+  gameEnded: boolean = false
+  canRetract: boolean = false
+  turn: number = 1
+  preTurn: number
+  status: number[][]
+  preStatus: number[][]
+  isValid: boolean[][]
+  preIsValid: boolean[][]
+  tileWidth: number
 
   constructor() { }
 
+  newGame() {
+    
+  }
+
   ngOnInit() {
+    // Get size and numOfPlayers from StarterComponent
+    let data = history.state.data
+    if (data) {
+      if ('size' in data) {
+        this.size = data['size']
+      }
+      if ('numOfPlayers' in data) {
+        this.numOfPlayers = data['numOfPlayers']
+      }
+    }
+    // Init game status
+    this.status = Array.from(Array(this.size), () => Array(this.size).fill(0))
+    this.preStatus = Array.from(Array(this.size), () => Array(this.size).fill(0))
+    this.isValid = Array.from(Array(this.size), () => Array(this.size).fill(false))
+    this.preIsValid = Array.from(Array(this.size), () => Array(this.size).fill(false))
+    // Set the edges to be a valid move
+    const EDGE_INDICES = [0, this.size - 1]
+    for (let rowIndex in this.isValid) {
+      for (let colIndex in this.isValid[rowIndex]) {
+        if (rowIndex in EDGE_INDICES ||
+            colIndex in EDGE_INDICES) {
+              this.isValid[rowIndex][colIndex] = true
+            }
+      }
+    }
+
+    const MARGIN = 2
+    let screenWidth = document.getElementById("wrapper").offsetWidth
+    this.tileWidth = Math.floor(screenWidth / this.size) - MARGIN * 2
+
+    console.log(this.tileWidth)
+
   }
 
 }
--- src/app/starter/starter.component.ts
+++ src/app/starter/starter.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-starter',
   templateUrl: './starter.component.html',
   styleUrls: ['./starter.component.scss']
 })
-export class StarterComponent implements OnInit {
 
-  constructor() { }
+export class StarterComponent implements OnInit {
+  sizeOptions: number[] = [...Array(8).keys()].map(x => x+=8)
+  numOfPlayersOptions: number[] = [...Array(5).keys()].map(x => x += 2)
+  size: number = 10
+  numOfPlayers: number = 4
+
+  
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    
+  }
+  
+  onChooseSize(size: number) {
+    this.size = size
   }
 
+  onChooseNumOfPlayers(numOfPlayers: number) {
+    this.numOfPlayers = numOfPlayers
+  }
+
+  newGame() {
+    this.router.navigate(['/game'], { state: { data: {
+      'size': this.size,
+      'numOfPlayers': this.numOfPlayers
+    } } })
+  }
+
+  onNewGame() {
+    swal({
+      title: "Are you sure to start a new game?",
+      text: "You will not be able to recover the game progress!",
+      icon: "info",
+      buttons: {
+        cancel: {
+          text: "Cancel",
+          value: null,
+          visible: true,
+          className: "",
+          closeModal: true,
+        },
+        confirm: {
+          text: "Yes, start a new game!",
+          value: true,
+          visible: true,
+          className: "",
+          closeModal: true
+        }
+      }
+    })
+    .then((value) => {
+      if (value) {
+        // new game new gameStatus
+        localStorage["gameStatus"] = ""
+        this.newGame()
+      }
+    })
+  }
 }
